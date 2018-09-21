bf9bf9bad1b044cb39cde7196e7b5146c3527e6a
Davide Mauri <damauri@microsoft.com>
2018-09-21 10:15:58 -0700

    set sas token ttl to 1 month

--- _common/generate-event-hub-sas-token.py
+++ _common/generate-event-hub-sas-token.py
@@ -10,7 +10,7 @@ import base64
 def get_auth_token(eh_namespace, eh_name, eh_key):
     uri = quote_plus("https://{0}.servicebus.windows.net/{1}".format(eh_namespace, eh_name))
     eh_key = eh_key.encode('utf-8')
-    expiry = str(int(time.time() + 10000))
+    expiry = str(int(time.time() + 60 * 60 * 24 * 31))
     string_to_sign = (uri + '\n' + expiry).encode('utf-8')
     signed_hmac_sha256 = hmac.HMAC(eh_key, string_to_sign, hashlib.sha256)
     signature = quote(base64.b64encode(signed_hmac_sha256.digest()))
