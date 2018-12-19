ade82a81df604abb240efb5237366bc17732acfc
Davide Mauri <damauri@microsoft.com>
2018-12-19 12:51:17 -0800

    updated to work with Azure Function v2

--- cosmos-db/build-streaming-processor.bat
+++ cosmos-db/build-streaming-processor.bat
@@ -1,2 +1,1 @@
-dotnet build .\StreamingProcessor-CosmosDB-Test0\StreamingProcessor-CosmosDB\ --configuration Release --framework net461
-dotnet build .\StreamingProcessor-CosmosDB-Test1\StreamingProcessor-CosmosDB\ --configuration Release --framework net461
+dotnet build .\StreamingProcessor-CosmosDB-Test1\StreamingProcessor-CosmosDB-Test1 --configuration Release
