88319b02df71848bf0798b5703439be46bba809e
Davide Mauri <damauri@microsoft.com>
2019-01-08 16:44:29 -0800

    removed .bat file as everything else is done via .sh

--- cosmos-db/build-streaming-processor.bat
+++
@@ -1,2 +0,0 @@
-dotnet build .\StreamingProcessor-CosmosDB-Test0\StreamingProcessor-CosmosDB --configuration Release
-dotnet build .\StreamingProcessor-CosmosDB-Test1\StreamingProcessor-CosmosDB --configuration Release
---
+++ cosmos-db/build-streaming-processor.sh
@@ -0,0 +1,2 @@
+dotnet build ./StreamingProcessor-CosmosDB-Test0/StreamingProcessor-CosmosDB --configuration Release
+dotnet build ./StreamingProcessor-CosmosDB-Test1/StreamingProcessor-CosmosDB --configuration Release
