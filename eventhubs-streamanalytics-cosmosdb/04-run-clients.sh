9f644580dd2bba0c879f50732b32b65960170d5a
Davide Mauri <damauri@microsoft.com>
2019-05-24 16:59:54 +0000

    corrected values

--- eventhubs-streamanalytics-cosmosdb/04-run-clients.sh
+++ eventhubs-streamanalytics-cosmosdb/04-run-clients.sh
@@ -76,13 +76,13 @@ echo " . hatch rate: $HATCH_RATE"
 curl http://$MASTER_IP:8089/swarm -X POST -F "locust_count=$USER_COUNT" -F "hatch_rate=$HATCH_RATE"
 
 echo 'done'
-echo 'starting to monitor locusts for 10 seconds... '
+echo 'starting to monitor locusts for 20 seconds... '
 sleep 5
 for s in {1..10} 
 do
     RPS=$(curl -s -X GET http://$MASTER_IP:8089/stats/requests | jq ".stats[0].current_rps")
     echo "locust is sending $RPS messages/sec"
-    sleep 1
+    sleep 2
 done
 echo 'monitoring done'
 
