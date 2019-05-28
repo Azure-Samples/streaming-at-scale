de160498ca503798ee1033537e80a2efaa0b4257
Davide Mauri <damauri@microsoft.com>
2019-05-28 11:03:07 -0700

    refactorings

--- eventhubs-streamanalytics-eventhubs/create-solution.sh
+++ eventhubs-streamanalytics-eventhubs/create-solution.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-set -e
-
-if [ -z $1 ]; then
-    echo "usage: $0 <deployment-name> <steps>"
-    echo "eg: $0 test1"    
-    exit 1
-fi
+set -euo pipefail
 
 on_error() {
     set +e
@@ -17,60 +11,100 @@ on_error() {
 
 trap 'on_error $LINENO' ERR
 
-export PREFIX=$1
-export RESOURCE_GROUP=$PREFIX
-export LOCATION=eastus
+usage() { 
+    echo "Usage: $0 -d <deployment-name> [-s <steps>] [-t <test-type>] [-l <location>]" 1>&2; 
+    echo "-s: specify which steps should be executed. Default=CIDPT" 1>&2; 
+    echo "    Possibile values:" 1>&2; 
+    echo "      C=COMMON" 1>&2; 
+    echo "      I=INGESTION" 1>&2; 
+    echo "      P=PROCESSING" 1>&2; 
+    echo "      T=TEST clients" 1>&2; 
+    echo "-t: test 1,5,10 thousands msgs/sec. Default=1"
+    echo "-l: where to create the resources. Default=eastus"
+    exit 1; 
+}
+
+export PREFIX=''
+export LOCATION=''
+export TESTTYPE=''
+export STEPS=''
+
+# Initialize parameters specified from command line
+while getopts ":d:s:t:l:" arg; do
+	case "${arg}" in
+		d)
+			PREFIX=${OPTARG}
+			;;
+		s)
+			STEPS=${OPTARG}
+			;;
+		t)
+			TESTTYPE=${OPTARG}
+			;;
+		l)
+			LOCATION=${OPTARG}
+			;;
+		esac
+done
+shift $((OPTIND-1))
+
+if [[ -z "$PREFIX" ]]; then
+	echo "Enter a name for this deployment."
+	usage
+fi
+
+if [[ -z "$LOCATION" ]]; then
+	export LOCATION="eastus"
+fi
+
+if [[ -z "$TESTTYPE" ]]; then
+	export TESTTYPE="1"
+fi
+
+if [[ -z "$STEPS" ]]; then
+	export STEPS="CIDPT"
+fi
 
 # 10000 messages/sec
-export EVENTHUB_PARTITIONS=12
-export EVENTHUB_CAPACITY=10
-export PROC_JOB_NAME=streamingjob
-export PROC_STREAMING_UNITS=12
-export TEST_CLIENTS=20
+if [ "$TESTTYPE" == "10" ]; then
+    export EVENTHUB_PARTITIONS=12
+    export EVENTHUB_CAPACITY=10
+    export PROC_JOB_NAME=streamingjob
+    export PROC_STREAMING_UNITS=12
+    export TEST_CLIENTS=30
+fi
 
 # 5500 messages/sec
-# export EVENTHUB_PARTITIONS=6
-# export EVENTHUB_CAPACITY=6
-# export PROC_JOB_NAME=streamingjob
-# export PROC_STREAMING_UNITS=6
-# export TEST_CLIENTS=10
+if [ "$TESTTYPE" == "5" ]; then
+    export EVENTHUB_PARTITIONS=6
+    export EVENTHUB_CAPACITY=6
+    export PROC_JOB_NAME=streamingjob
+    export PROC_STREAMING_UNITS=6
+    export TEST_CLIENTS=16
+fi
 
 # 1000 messages/sec
-# export EVENTHUB_PARTITIONS=2
-# export EVENTHUB_CAPACITY=2
-# export PROC_JOB_NAME=streamingjob
-# export PROC_STREAMING_UNITS=3
-# export TEST_CLIENTS=2
-
-export STEPS=$2
-if [ -z $PROC_STREAMING_UNITS ]; then  
-    let "PROC_STREAMING_UNITS=EVENTHUB_PARTITIONS"
+if [ "$TESTTYPE" == "1" ]; then
+    export EVENTHUB_PARTITIONS=2
+    export EVENTHUB_CAPACITY=2
+    export PROC_JOB_NAME=streamingjob
+    export PROC_STREAMING_UNITS=3
+    export TEST_CLIENTS=3
 fi
 
-if [ -z $STEPS ]; then  
-    export STEPS="CIDPT"    
+# last checks and variables setup
+if [ -z ${TEST_CLIENTS+x} ]; then
+    usage
 fi
+
+export RESOURCE_GROUP=$PREFIX
 
 # remove log.txt if exists
 rm -f log.txt
 
-echo
-echo "Streaming at Scale with Stream Analytics and Event Hubs"
-echo "================================"
-echo
+echo "Checking pre-requisites..."
 
-echo "steps to be executed: $STEPS"
-echo
-
-echo "configuration: "
-echo "EventHubs       => TU: $EVENTHUB_CAPACITY, Partitions: $EVENTHUB_PARTITIONS"
-echo "StreamAnalytics => Name: $PROC_JOB_NAME, SU: $PROC_STREAMING_UNITS"
-echo "Locusts         => $TEST_CLIENTS"
-echo
-
-echo "Checking prerequisites..."
-
-HAS_AZ=`command -v az`
+HAS_AZ=$(command -v az)
 if [ -z HAS_AZ ]; then
     echo "AZ CLI not found"
     echo "please install it as described here:"
@@ -78,10 +112,23 @@ if [ -z HAS_AZ ]; then
     exit 1
 fi
 
-echo "deployment started..."
+echo
+echo "Streaming at Scale with Stream Analytics and Event Hubs"
+echo "======================================================="
 echo
 
-echo "***** [C] setting up common resources"
+echo "Steps to be executed: $STEPS"
+echo
+
+echo "Configuration: "
+echo ". Resource Group  => $RESOURCE_GROUP"
+echo ". Region          => $LOCATION"
+echo ". EventHubs       => TU: $EVENTHUB_CAPACITY, Partitions: $EVENTHUB_PARTITIONS"
+echo ". StreamAnalytics => Name: $PROC_JOB_NAME, SU: $PROC_STREAMING_UNITS"
+echo ". Locusts         => $TEST_CLIENTS"
+echo
+
+echo "***** [C] Setting up common resources"
 
     export AZURE_STORAGE_ACCOUNT=$PREFIX"storage"
 
@@ -92,10 +139,10 @@ echo "***** [C] setting up common resources"
     fi
 echo 
 
-echo "***** [I] setting up INGESTION AND EGRESS EVENT HUBS"
+echo "***** [I] Setting up INGESTION AND EGRESS EVENT HUBS"
     
-    export EVENTHUB_NAMESPACE=$PREFIX"ingest"
-    export EVENTHUB_NAME=$PREFIX"ingest-"$EVENTHUB_PARTITIONS
+    export EVENTHUB_NAMESPACE=$PREFIX"eventhubs"
+    export EVENTHUB_NAME=$PREFIX"in-"$EVENTHUB_PARTITIONS
     export EVENTHUB_NAME_OUT=$PREFIX"out-"$EVENTHUB_PARTITIONS
     export EVENTHUB_CG="asa"
 
@@ -105,7 +152,7 @@ echo "***** [I] setting up INGESTION AND EGRESS EVENT HUBS"
     fi
 echo
 
-echo "***** [P] setting up PROCESSING"
+echo "***** [P] Setting up PROCESSING"
 
     export PROC_JOB_NAME=$PREFIX"streamingjob"
     RUN=`echo $STEPS | grep P -o || true`
@@ -114,7 +161,7 @@ echo "***** [P] setting up PROCESSING"
     fi
 echo
 
-echo "***** [T] starting up TEST clients"
+echo "***** [T] Starting up TEST clients"
 
     export LOCUST_DNS_NAME=$PREFIX"locust"
 
