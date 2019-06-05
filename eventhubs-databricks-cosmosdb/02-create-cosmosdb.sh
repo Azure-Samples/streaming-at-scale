cd3b80a8517273df5832539030fdb66e7814c2c2
Davide Mauri <damauri@microsoft.com>
2019-06-04 18:01:59 -0700

    Revert "databricks cosmosdb sample"
    This reverts commit f6838bfdc789613cb8230e3f1a9d3f31881a2c23.

--- eventhubs-databricks-cosmosdb/02-create-cosmosdb.sh
+++ eventhubs-databricks-cosmosdb/02-create-cosmosdb.sh
@@ -5,7 +5,7 @@ echo ". name: $COSMOSDB_SERVER_NAME"
 SERVER_EXISTS=`az cosmosdb check-name-exists -n $COSMOSDB_SERVER_NAME -o tsv`
 if [ $SERVER_EXISTS == "false" ]; then
     az cosmosdb create -g $RESOURCE_GROUP -n $COSMOSDB_SERVER_NAME \
-        -o tsv >> log.txt
+    -o tsv >> log.txt
 fi
 
 echo 'creating cosmosdb database'
@@ -13,7 +13,7 @@ echo ". name: $COSMOSDB_DATABASE_NAME"
 DB_EXISTS=`az cosmosdb database exists -g $RESOURCE_GROUP -n $COSMOSDB_SERVER_NAME --db-name $COSMOSDB_DATABASE_NAME -o tsv`
 if [ $DB_EXISTS == "false" ]; then
     az cosmosdb database create -g $RESOURCE_GROUP -n $COSMOSDB_SERVER_NAME --db-name $COSMOSDB_DATABASE_NAME \
-        -o tsv >> log.txt
+    -o tsv >> log.txt
 fi
 
 echo 'creating cosmosdb collection'
@@ -21,8 +21,8 @@ echo ". name: $COSMOSDB_COLLECTION_NAME"
 COLLECTION_EXISTS=`az cosmosdb collection exists -g $RESOURCE_GROUP -n $COSMOSDB_SERVER_NAME --db-name $COSMOSDB_DATABASE_NAME --collection-name $COSMOSDB_COLLECTION_NAME -o tsv`
 if [ $COLLECTION_EXISTS == "false" ]; then
     az cosmosdb collection create -g $RESOURCE_GROUP -n $COSMOSDB_SERVER_NAME \
-        -d $COSMOSDB_DATABASE_NAME --collection-name $COSMOSDB_COLLECTION_NAME \
-        --partition-key-path "/deviceId" --throughput $COSMOSDB_RU \
-        --indexing-policy @index-policy.json \
-        -o tsv >> log.txt
+    -d $COSMOSDB_DATABASE_NAME --collection-name $COSMOSDB_COLLECTION_NAME \
+    --partition-key-path "/deviceId" --throughput $COSMOSDB_RU \
+    --indexing-policy @index-policy.json \
+    -o tsv >> log.txt
 fi
--- eventhubs-databricks-cosmosdb/create-solution.sh
+++ eventhubs-databricks-cosmosdb/create-solution.sh
@@ -1,114 +1,58 @@
 #!/bin/bash
 
-set -euo pipefail
+set -e
+
+if [ -z $1 ]; then
+    echo "usage: $0 <deployment-name> <steps>"
+    echo "eg: $0 test1"    
+    exit 1
+fi
 
 on_error() {
     set +e
     echo "There was an error, execution halted" >&2
-    echo "Error at line $1"
     exit 1
 }
 
-trap 'on_error $LINENO' ERR
+trap on_error ERR
 
-usage() { 
-    echo "Usage: $0 -d <deployment-name> [-s <steps>] [-t <test-type>] [-l <location>]" 1>&2; 
-    echo "-s: specify which steps should be executed. Default=CIDPT" 1>&2; 
-    echo "    Possibile values:" 1>&2; 
-    echo "      C=COMMON" 1>&2; 
-    echo "      I=INGESTION" 1>&2; 
-    echo "      D=DATABASE" 1>&2; 
-    echo "      P=PROCESSING" 1>&2; 
-    echo "      T=TEST clients" 1>&2; 
-    echo "-t: test 1,5,10 thousands msgs/sec. Default=1"
-    echo "-l: where to create the resources. Default=eastus"
-    exit 1; 
-}
-
-export PREFIX=''
-export LOCATION=''
-export TESTTYPE=''
-export STEPS=''
-
-# Initialize parameters specified from command line
-while getopts ":d:s:t:l:" arg; do
-	case "${arg}" in
-		d)
-			PREFIX=${OPTARG}
-			;;
-		s)
-			STEPS=${OPTARG}
-			;;
-		t)
-			TESTTYPE=${OPTARG}
-			;;
-		l)
-			LOCATION=${OPTARG}
-			;;
-		esac
-done
-shift $((OPTIND-1))
-
-if [[ -z "$PREFIX" ]]; then
-	echo "Enter a name for this deployment."
-	usage
-fi
-
-if [[ -z "$LOCATION" ]]; then
-	export LOCATION="eastus"
-fi
-
-if [[ -z "$TESTTYPE" ]]; then
-	export TESTTYPE="1"
-fi
-
-if [[ -z "$STEPS" ]]; then
-	export STEPS="CIDPT"
-fi
-
-# 10000 messages/sec
-if [ "$TESTTYPE" == "10" ]; then
-    export EVENTHUB_PARTITIONS=12
-    export EVENTHUB_CAPACITY=12
-    export PROC_JOB_NAME=streamingjob
-    export PROC_STREAMING_UNITS=36 # must be 1, 3, 6 or a multiple or 6
-    export COSMOSDB_RU=100000
-    export TEST_CLIENTS=30
-fi
-
-# 5500 messages/sec
-if [ "$TESTTYPE" == "5" ]; then
-    export EVENTHUB_PARTITIONS=8
-    export EVENTHUB_CAPACITY=6
-    export PROC_JOB_NAME=streamingjob
-    export PROC_STREAMING_UNITS=24 # must be 1, 3, 6 or a multiple or 6
-    export COSMOSDB_RU=60000
-    export TEST_CLIENTS=16
-fi
+export PREFIX=$1
+export RESOURCE_GROUP=$PREFIX
+export LOCATION=eastus
 
 # 1000 messages/sec
-if [ "$TESTTYPE" == "1" ]; then
-    export EVENTHUB_PARTITIONS=2
-    export EVENTHUB_CAPACITY=2
-    export PROC_JOB_NAME=streamingjob
-    export PROC_STREAMING_UNITS=6 # must be 1, 3, 6 or a multiple or 6
-    export COSMOSDB_RU=20000
-    export TEST_CLIENTS=3 
-fi
+export EVENTHUB_PARTITIONS=2
+export EVENTHUB_CAPACITY=2
+export COSMOSDB_RU=20000
+export TEST_CLIENTS=2
 
-# last checks and variables setup
-if [ -z ${TEST_CLIENTS+x} ]; then
-    usage
-fi
+export STEPS=$2
 
-export RESOURCE_GROUP=$PREFIX
+if [ -z $STEPS ]; then  
+    export STEPS="CIDPT"    
+fi
 
 # remove log.txt if exists
 rm -f log.txt
 
-echo "Checking pre-requisites..."
+echo
+echo "Streaming at Scale: EventHubs + Databricks + CosmosDB "
+echo "======================================================"
+echo
 
-HAS_AZ=$(command -v az)
+echo "steps to be executed: $STEPS"
+echo
+
+echo "configuration: "
+echo "EventHubs  => TU: $EVENTHUB_CAPACITY, Partitions: $EVENTHUB_PARTITIONS"
+#echo "Function   => Name: $PROC_FUNCTION, SKU: $PROC_FUNCTION_SKU, Workers: $PROC_FUNCTION_WORKERS"
+echo "CosmosDB   => RU: $COSMOSDB_RU"
+echo "Locusts    => $TEST_CLIENTS"
+echo
+
+echo "checking prerequisistes..."
+
+HAS_AZ=`command -v az`
 if [ -z HAS_AZ ]; then
     echo "AZ CLI not found"
     echo "please install it as described here:"
@@ -116,59 +60,45 @@ if [ -z HAS_AZ ]; then
     exit 1
 fi
 
-HAS_JQ=$(command -v jq)
-if [ -z HAS_JQ ]; then
-    echo "jq not found"
-    echo "please install it using your package manager, for example, on Ubuntu:"
-    echo "  sudo apt install jq"
-    echo "or as described here:"
-    echo "  https://stedolan.github.io/jq/download/"
+HAS_PY3=`command -v python3`
+if [ -z HAS_PY3 ]; then
+    echo "python3 not found"
+    echo "please install it as it is needed by the script"
+    exit 1
+fi
+
+HAS_ZIP=`command -v zip`
+if [ -z HAS_ZIP ]; then
+    echo "zip not found"
+    echo "please install it as it is needed by the script"
     exit 1
 fi
 
 HAS_DATABRICKSCLI=`command -v databricks`
 if [ -z HAS_DATABRICKSCLI ]; then
-    echo "databricks-cli not found"
-    echo "please install it as described here:"
-    echo "https://github.com/databricks/databricks-cli"
+    echo "databricks not found"
+    echo "please install it as it is needed by the script"
     exit 1
 fi
 
-echo
-echo "Streaming at Scale with Azure Databricks and CosmosDB"
-echo "====================================================="
+echo "deployment started..."
 echo
 
-echo "Steps to be executed: $STEPS"
-echo
-
-echo "Configuration: "
-echo ". Resource Group  => $RESOURCE_GROUP"
-echo ". Region          => $LOCATION"
-echo ". EventHubs       => TU: $EVENTHUB_CAPACITY, Partitions: $EVENTHUB_PARTITIONS"
-echo ". Databricks      => ***TODO***"
-echo ". CosmosDB        => RU: $COSMOSDB_RU"
-echo ". Locusts         => $TEST_CLIENTS"
-echo
-
-echo "Deployment started..."
-echo
-
-echo "***** [C] Setting up COMMON resources"
+echo "***** [C] setting up common resources"
 
     export AZURE_STORAGE_ACCOUNT=$PREFIX"storage"
 
-    RUN=`echo $STEPS | grep C -o || true`
+    RUN=`echo $STEPS | grep C -o || true`      
     if [ ! -z $RUN ]; then
         ../_common/01-create-resource-group.sh
         ../_common/02-create-storage-account.sh
     fi
 echo 
 
-echo "***** [I] Setting up INGESTION"
+echo "***** [I] setting up INGESTION"
     
-    export EVENTHUB_NAMESPACE=$PREFIX"eventhubs"    
-    export EVENTHUB_NAME=$PREFIX"in-"$EVENTHUB_PARTITIONS
+    export EVENTHUB_NAMESPACE=$PREFIX"ingest"    
+    export EVENTHUB_NAME=$PREFIX"ingest-"$EVENTHUB_PARTITIONS
     export EVENTHUB_CG="cosmos"
 
     RUN=`echo $STEPS | grep I -o || true`
@@ -177,7 +107,7 @@ echo "***** [I] Setting up INGESTION"
     fi
 echo
 
-echo "***** [D] Setting up DATABASE"
+echo "***** [D] setting up DATABASE"
 
     export COSMOSDB_SERVER_NAME=$PREFIX"cosmosdb" 
     export COSMOSDB_DATABASE_NAME="streaming"
@@ -189,25 +119,30 @@ echo "***** [D] Setting up DATABASE"
     fi
 echo
 
-echo "***** [P] Setting up PROCESSING"
+echo "***** [P] setting up PROCESSING"
 
-    export ADB_WORKSPACE=$PREFIX"databricks" 
-    
-    RUN=`echo $STEPS | grep P -o || true`
-    if [ ! -z $RUN ]; then
-        ./03-create-databricks.sh
-    fi
+    # export PROC_FUNCTION_APP_NAME=$PREFIX"process"
+    # export PROC_FUNCTION_NAME=StreamingProcessor
+    # export PROC_PACKAGE_FOLDER=.
+    # export PROC_PACKAGE_TARGET=CosmosDB    
+    # export PROC_PACKAGE_NAME=$PROC_FUNCTION_NAME-$PROC_PACKAGE_TARGET.zip
+    # export PROC_PACKAGE_PATH=$PROC_PACKAGE_FOLDER/$PROC_PACKAGE_NAME
+
+    #RUN=`echo $STEPS | grep P -o || true`
+    #if [ ! -z $RUN ]; then
+        #./03-create-processing-function.sh
+        #./04-configure-processing-function-cosmosdb.sh
+    #fi
 echo
 
-echo "***** [T] Starting up TEST clients"
+echo "***** [T] starting up TEST clients"
 
     export LOCUST_DNS_NAME=$PREFIX"locust"
 
     RUN=`echo $STEPS | grep T -o || true`
     if [ ! -z $RUN ]; then
-        ./04-run-clients.sh
+        ./05-run-clients.sh
     fi
 echo
 
-echo "***** Done"
-
+echo "***** done"
