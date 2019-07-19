fe6bebdd348c02a26c2040e98ef5076a7b35e07a
algattik <algattik@microsoft.com>
2019-07-19 20:46:28 +0200

    Replaced bacpac deployment with SQL script

--- components/azure-sql-database/create-sql-database.sh
+++ components/azure-sql-database/create-sql-database.sh
@@ -4,52 +4,58 @@ set -euo pipefail
 
 echo "retrieving storage connection string"
 AZURE_STORAGE_CONNECTION_STRING=$(az storage account show-connection-string --name $AZURE_STORAGE_ACCOUNT -g $RESOURCE_GROUP -o tsv)
-
-echo "creating 'database' container"
-az storage container create --connection-string $AZURE_STORAGE_CONNECTION_STRING --name database \
-    -o tsv >> log.txt
-
-echo "uploading bacpac"
-az storage blob upload --connection-string $AZURE_STORAGE_CONNECTION_STRING --container-name database --name streaming.bacpac --file "../components/azure-sql-database/streaming.bacpac" \
-    -o tsv >> log.txt
-
-echo "retrieving blob url"
-BLOB_URL="$(az storage blob url --connection-string $AZURE_STORAGE_CONNECTION_STRING --container-name database --name streaming.bacpac -o tsv)"
-
-echo "retrieving container sas"
-#Fall back to OSX date syntax if Linux syntax fails.
-EXPIRE_ON=$(date -u -d "30 minutes" '+%Y-%m-%dT%H:%MZ' 2>/dev/null || date -u '-v+30M' '+%Y-%m-%dT%H:%MZ')
-BLOB_SAS="?$(az storage container generate-sas --connection-string $AZURE_STORAGE_CONNECTION_STRING --name database --permissions lr --https-only --expiry $EXPIRE_ON -o tsv)"
+AZURE_STORAGE_KEY=$(az storage account keys list -g $RESOURCE_GROUP -n $AZURE_STORAGE_ACCOUNT -o tsv --query "[0].value")
 
 echo "deploying azure sql"
 echo ". server: $SQL_SERVER_NAME"
 echo ". database: $SQL_DATABASE_NAME"
 
-SERVER_EXIST=$(az sql server list -g $RESOURCE_GROUP -o tsv --query "[].name" | grep $SQL_SERVER_NAME || true)
-if [ "${SERVER_EXIST}" == "$SQL_SERVER_NAME" ]; then
-    echo "server already exits: $SERVER_EXIST"
-    echo "checking if database already exists too"
-    DB_EXISTS=$(az sql db list -g $RESOURCE_GROUP -s $SQL_SERVER_NAME -o tsv --query "[].name" | grep $SQL_DATABASE_NAME || true)
-    if [ "${DB_EXISTS}" == "$SQL_DATABASE_NAME" ]; then
-        echo "database already exits: $SQL_DATABASE_NAME"
-        echo "deleting existing database"
-        az sql db delete -g $RESOURCE_GROUP -s $SQL_SERVER_NAME -n $SQL_DATABASE_NAME -y \
-        -o tsv >> log.txt
-    fi
-fi
+# Create a logical server in the resource group
+az sql server create \
+    --name $SQL_SERVER_NAME \
+    --resource-group $RESOURCE_GROUP \
+    --admin-user serveradmin \
+    --admin-password "$SQL_ADMIN_PASS" \
+    -o tsv >> log.txt
 
-echo "deploying arm template"
-az group deployment create \
-    --name "$RESOURCE_GROUP-AzureSQL" \
-    --resource-group "$RESOURCE_GROUP" \
-    --template-file "../components/azure-sql-database/sql-database-arm-template.json" \
-    --parameters \
-        ServerName=$SQL_SERVER_NAME \
-        DatabaseName=$SQL_DATABASE_NAME \
-	    AdminLogin="serveradmin" \
-	    AdminLoginPassword=$SQL_ADMIN_PASS \
-        DacPacPath=$BLOB_URL \
-        DacPacContainerSAS=$BLOB_SAS \
-        SKU=$SQL_SKU \
-	--verbose \
+echo "Enabling access from Azure"
+# Configure a firewall rule for the server
+az sql server firewall-rule create \
+    --resource-group $RESOURCE_GROUP \
+    --server $SQL_SERVER_NAME \
+    -n AllowAllWindowsAzureIps \
+    --start-ip-address 0.0.0.0 \
+    --end-ip-address 0.0.0.0 \
+    -o tsv >> log.txt
+
+echo "deploying SQL database"
+az sql db create --resource-group "$RESOURCE_GROUP" \
+    --server $SQL_SERVER_NAME \
+    --name $SQL_DATABASE_NAME \
+    --service-objective $SQL_SKU \
+    -o tsv >> log.txt
+
+echo 'creating file share'
+az storage share create -n sqlprovision --connection-string $AZURE_STORAGE_CONNECTION_STRING \
+    -o tsv >> log.txt
+
+echo 'uploading provisioning scripts'
+az storage file upload-batch --source ../components/azure-sql-database/provision \
+    --destination sqlprovision --connection-string $AZURE_STORAGE_CONNECTION_STRING \
+    -o tsv >> log.txt
+
+echo 'running provisioning scripts in container instance'
+az container create -g $RESOURCE_GROUP -n sqlprovision \
+    --image mcr.microsoft.com/mssql-tools:v1 \
+    --azure-file-volume-account-name $AZURE_STORAGE_ACCOUNT --azure-file-volume-account-key $AZURE_STORAGE_KEY \
+    --azure-file-volume-share-name sqlprovision --azure-file-volume-mount-path /sqlprovision \
+    --command-line "bash /sqlprovision/provision.sh" \
+    --environment-variables SQL_SERVER_NAME=$SQL_SERVER_NAME SQL_DATABASE_NAME=$SQL_DATABASE_NAME \
+    --secure-environment-variables SQL_ADMIN_PASS="$SQL_ADMIN_PASS" \
+    --cpu 1 --memory 1 \
+    --restart-policy Never \
+    -o tsv >> log.txt
+
+echo 'deleting container instance'
+az container delete -g $RESOURCE_GROUP -n sqlprovision-03 --yes \
     -o tsv >> log.txt
