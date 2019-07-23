f55c244e556f638258b42572f42dcd8afe380ec4
algattik <algattik@microsoft.com>
2019-07-23 19:51:31 +0200

    Created dataexplorer solution

---
+++ eventhubs-dataexplorer/create-solution.sh
@@ -0,0 +1,173 @@
+#!/bin/bash
+
+# Strict mode, fail on any error
+set -euo pipefail
+
+export PREFIX=''
+export LOCATION="eastus"
+export TESTTYPE="1"
+export STEPS="CIDTM"
+
+usage() { 
+    echo "Usage: $0 -d <deployment-name> [-s <steps>] [-t <test-type>] [-l <location>]"
+    echo "-s: specify which steps should be executed. Default=$STEPS"
+    echo "    Possible values:"
+    echo "      C=COMMON"
+    echo "      I=INGESTION"
+    echo "      P=PROCESSING"
+    echo "      T=TEST clients"
+    echo "      M=METRICS reporting"
+    echo "      V=VERIFY deployment"
+    echo "-t: test 1,5,10 thousands msgs/sec. Default=$TESTTYPE"
+    echo "-l: where to create the resources. Default=$LOCATION"
+    exit 1; 
+}
+
+# Initialize parameters specified from command line
+while getopts ":d:s:t:l:" arg; do
+	case "${arg}" in
+		d)
+			PREFIX=${OPTARG}
+			;;
+		s)
+			STEPS=${OPTARG}
+			;;
+		t)
+			TESTTYPE=${OPTARG}
+			;;
+		l)
+			LOCATION=${OPTARG}
+			;;
+		esac
+done
+shift $((OPTIND-1))
+
+if [[ -z "$PREFIX" ]]; then
+	echo "Enter a name for this deployment."
+	usage
+fi
+
+# 10000 messages/sec
+if [ "$TESTTYPE" == "10" ]; then
+    export EVENTHUB_PARTITIONS=12
+    export EVENTHUB_CAPACITY=12
+    export DATAEXPLORER_SKU=D11_v2
+    export TEST_CLIENTS=30
+fi
+
+# 5000 messages/sec
+if [ "$TESTTYPE" == "5" ]; then
+    export EVENTHUB_PARTITIONS=8
+    export EVENTHUB_CAPACITY=6
+    export DATAEXPLORER_SKU=D11_v2
+    export TEST_CLIENTS=16
+fi
+
+# 1000 messages/sec
+if [ "$TESTTYPE" == "1" ]; then
+    export EVENTHUB_PARTITIONS=2
+    export EVENTHUB_CAPACITY=2
+    export DATAEXPLORER_SKU=D11_v2
+    export PROC_STREAMING_UNITS=6 # must be 1, 3, 6 or a multiple or 6
+    export TEST_CLIENTS=3 
+fi
+
+# last checks and variables setup
+if [ -z ${TEST_CLIENTS+x} ]; then
+    usage
+fi
+
+export RESOURCE_GROUP=$PREFIX
+
+# remove log.txt if exists
+rm -f log.txt
+
+echo "Checking pre-requisites..."
+
+source ../assert/has-local-az.sh
+source ../assert/has-local-jq.sh
+
+echo
+echo "Streaming at Scale with Azure Data Explorer"
+echo "==========================================="
+echo
+
+echo "Steps to be executed: $STEPS"
+echo
+
+echo "Configuration: "
+echo ". Resource Group  => $RESOURCE_GROUP"
+echo ". Region          => $LOCATION"
+echo ". EventHubs       => TU: $EVENTHUB_CAPACITY, Partitions: $EVENTHUB_PARTITIONS"
+echo ". Data Explorer   => SKU: $DATAEXPLORER_SKU"
+echo ". Locusts         => $TEST_CLIENTS"
+echo
+
+echo "Deployment started..."
+echo
+
+echo "***** [C] Setting up COMMON resources"
+
+    export AZURE_STORAGE_ACCOUNT=$PREFIX"storage"
+
+    RUN=`echo $STEPS | grep C -o || true`
+    if [ ! -z "$RUN" ]; then
+        source ../components/azure-common/create-resource-group.sh
+        source ../components/azure-storage/create-storage-account.sh
+    fi
+echo 
+
+echo "***** [I] Setting up INGESTION"
+    
+    export EVENTHUB_NAMESPACE=$PREFIX"eventhubs"    
+    export EVENTHUB_NAME=$PREFIX"in-"$EVENTHUB_PARTITIONS
+    export EVENTHUB_CG="dataexplorer"
+
+    RUN=`echo $STEPS | grep I -o || true`
+    if [ ! -z "$RUN" ]; then
+        source ../components/azure-event-hubs/create-event-hub.sh
+    fi
+echo
+
+echo "***** [D] Setting up DATABASE"
+
+    export DATAEXPLORER_CLUSTER=$PREFIX"adx" 
+    export DATAEXPLORER_DATABASE="streaming"
+    export DATAEXPLORER_CLIENT_NAME=$DATAEXPLORER_CLUSTER"-reader9"
+    export DATAEXPLORER_KEYVAULT=$DATAEXPLORER_CLUSTER"kv"
+
+    RUN=`echo $STEPS | grep D -o || true`
+    if [ ! -z "$RUN" ]; then
+        source ../components/azure-dataexplorer/create-dataexplorer.sh
+    fi
+echo
+
+echo "***** [T] Starting up TEST clients"
+
+    RUN=`echo $STEPS | grep T -o || true`
+    if [ ! -z "$RUN" ]; then
+        source ../simulator/run-event-generator.sh
+    fi
+echo
+
+echo "***** [M] Starting METRICS reporting"
+
+    RUN=`echo $STEPS | grep M -o || true`
+    if [ ! -z "$RUN" ]; then
+        source ../components/azure-event-hubs/report-throughput.sh
+    fi
+echo
+
+echo "***** [V] Starting deployment VERIFICATION"
+
+    export ADB_WORKSPACE=$PREFIX"databricks" 
+    export ADB_TOKEN_KEYVAULT=$PREFIX"kv" #NB AKV names are limited to 24 characters
+
+    RUN=`echo $STEPS | grep V -o || true`
+    if [ ! -z "$RUN" ]; then
+        source ../components/azure-databricks/create-databricks.sh
+        source ../streaming/databricks/runners/verify-dataexplorer.sh
+    fi
+echo
+
+echo "***** Done"
---
+++ streaming/databricks/notebooks/verify-dataexplorer.scala
@@ -0,0 +1,49 @@
+// Databricks notebook source
+dbutils.widgets.text("dataexplorer-cluster", "https://MYCLUSTER.northeurope.kusto.windows.net")
+dbutils.widgets.text("dataexplorer-database", "streaming")
+dbutils.widgets.text("dataexplorer-query", "EventTable")
+dbutils.widgets.text("dataexplorer-client-id", "MYCLIENTID")
+dbutils.widgets.text("dataexplorer-storage-account", "MYSTORAGE")
+dbutils.widgets.text("dataexplorer-storage-container", "dataexplorer")
+dbutils.widgets.text("assert-events-per-second", "900", "Assert min events per second (computed over 1 min windows)")
+dbutils.widgets.text("assert-latency-milliseconds", "15000", "Assert max latency in milliseconds (averaged over 1 min windows)")
+
+// COMMAND ----------
+
+import com.microsoft.kusto.spark.datasource.KustoSourceOptions
+import com.microsoft.kusto.spark.sql.extension.SparkExtension._
+import org.apache.spark.SparkConf
+import org.apache.spark.sql._
+
+val cluster = dbutils.widgets.get("dataexplorer-cluster")
+val database = dbutils.widgets.get("dataexplorer-database")
+val query = dbutils.widgets.get("dataexplorer-query")
+val conf: Map[String, String] = Map(
+  KustoSourceOptions.KUSTO_AAD_CLIENT_ID -> dbutils.widgets.get("dataexplorer-client-id"),
+  KustoSourceOptions.KUSTO_AAD_CLIENT_PASSWORD -> dbutils.secrets.get(scope = "MAIN", key = "dataexplorer-client-password"),
+  KustoSourceOptions.KUSTO_BLOB_STORAGE_ACCOUNT_NAME -> dbutils.widgets.get("dataexplorer-storage-account"),
+  KustoSourceOptions.KUSTO_BLOB_STORAGE_ACCOUNT_KEY -> dbutils.secrets.get(scope = "MAIN", key = "dataexplorer-storage-key"),
+  KustoSourceOptions.KUSTO_BLOB_CONTAINER -> dbutils.widgets.get("dataexplorer-storage-container")
+)
+val data = spark
+  .read
+  .kusto(cluster, database, query, conf)
+
+// COMMAND ----------
+
+import java.util.UUID.randomUUID
+
+val tempTable = "tempresult_" + randomUUID().toString.replace("-","_")
+
+data
+  .withColumn("enqueuedAt", $"createdAt")
+  .withColumn("storedAt", $"createdAt")
+  .createOrReplaceGlobalTempView(tempTable)
+
+// COMMAND ----------
+
+dbutils.notebook.run("verify-common", 0, Map(
+    "input-table" -> (spark.conf.get("spark.sql.globalTempDatabase") + "." + tempTable),
+    "assert-events-per-second" -> dbutils.widgets.get("assert-events-per-second"),
+    "assert-latency-milliseconds" -> dbutils.widgets.get("assert-latency-milliseconds")
+))
