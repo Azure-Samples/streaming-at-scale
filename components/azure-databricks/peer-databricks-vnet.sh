fa006a54667b95e4847565ac923939ae94d439c3
Alexandre Gattiker <algattik@Gold.local>
2019-07-30 14:26:59 +0200

    Update peer-databricks-vnet.sh

--- components/azure-databricks/peer-databricks-vnet.sh
+++ components/azure-databricks/peer-databricks-vnet.sh
@@ -9,11 +9,18 @@ echo "Getting VNET ids"
 databricks_vnet_name="databricks-vnet"
 databricks_vnet_id=$(az network vnet show -g $databricksResourceGroup -n $databricks_vnet_name --query id --out tsv)
 hdinsight_vnet_id=$(az network vnet show -g $RESOURCE_GROUP -n $VNET_NAME --query id --out tsv)
+hdinsight_peering_name="HDInsightToDatabricks-$databricksResourceGroup-$databricks_vnet_name"
 
 if az network vnet peering show -g $databricksResourceGroup -n "DatabricksToHDInsight" --vnet-name $databricks_vnet_name -o none 2> /dev/null; then
   echo "Deleting Databricks VNet peering"
   az network vnet peering delete -g $databricksResourceGroup -n "DatabricksToHDInsight" --vnet-name $databricks_vnet_name
 fi
+
+if az network vnet peering show -g $RESOURCE_GROUP -n "$hdinsight_peering_name" --vnet-name $VNET_NAME -o none 2> /dev/null; then
+  echo "Deleting HDInsight VNet peering"
+  az network vnet peering delete -g $RESOURCE_GROUP -n "$hdinsight_peering_name" --vnet-name $VNET_NAME
+fi
+
 echo "Peering Databricks VNet to HDInsight VNet"
 az network vnet peering create -g $databricksResourceGroup -n "DatabricksToHDInsight" \
   --vnet-name $databricks_vnet_name --remote-vnet $hdinsight_vnet_id \
@@ -21,6 +28,6 @@ az network vnet peering create -g $databricksResourceGroup -n "DatabricksToHDIns
   -o tsv >> log.txt
 
 echo "Peering HDInsight VNet to Databricks VNet"
-az network vnet peering create -g $RESOURCE_GROUP -n "HDInsightToDatabricks-$databricksResourceGroup-$databricks_vnet_name" \
+az network vnet peering create -g $RESOURCE_GROUP -n "$hdinsight_peering_name" \
   --vnet-name $VNET_NAME --remote-vnet $databricks_vnet_id \
   -o tsv >> log.txt
