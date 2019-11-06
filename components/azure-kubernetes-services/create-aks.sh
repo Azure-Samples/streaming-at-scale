4fc76acab1cda5074c103060ed1d281015fc03bf
Guilherme Lucas da Silva <gulucasd@microsoft.com>
2019-11-06 14:49:16 -0800

    ADD: adding all the steps for aks creation

--- components/azure-kubernetes-services/create-aks.sh
+++ components/azure-kubernetes-services/create-aks.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-AKS_CLUSTER_NAME='test-cluster'
-RESOURCE_GROUP='test-cluster'
-NODE_COUNT='3'
-VM_SIZE='Standard_DS2_v2'
+echo 'Creating aks cluster'
 
 # creating aks cluster
-az aks create -n $AKS_CLUSTER_NAME -g $RESOURCE_GROUP --node-count $NODE_COUNT --node-vm-size $VM_SIZE
+az aks create --name $AKS_CLUSTER_NAME --resource-group $RESOURCE_GROUP --node-count $NODE_COUNT --node-vm-size $VM_SIZE --generate-ssh-keys --service-principal $SERVICE_PRINCIPAL --client-secret $SERVICE_PRINCIPAL_SECRET
+
+echo 'Getting credentials'
 
 # get credentials for kubernetes
 az aks get-credentials -n $AKS_CLUSTER_NAME -g $RESOURCE_GROUP
 
+echo 'Initing helm inside the cluster'
+
 # installing helm inside cluster
 helm init
+
+echo 'Fixing permission problems for tiller'
 
 # fixing authorizations for tiller inside aks
 kubectl create serviceaccount --namespace kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
 kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
+
+echo 'Creating kubectl namespace'
 
 # following the repo instructions, this deploys the operator with helm
 kubectl create namespace kafka
@@ -27,6 +32,8 @@ helm install strimzi/strimzi-kafka-operator --namespace kafka --name kafka-opera
 # clone the repo in order to deploy kafka on aks
 git clone https://github.com/cnadolny/azure-kafka-kubernetes.git
 
+echo 'Creating kafka inside kubernetes'
+
 # installing all the yaml files from the repo inside aks
 kubectl create -n kafka -f azure-kafka-kubernetes/kafka-operator-strimzi/tls-kafka.yaml
 kubectl create -n kafka -f azure-kafka-kubernetes/kafka-operator-strimzi/kafka-topics.yaml
@@ -35,3 +42,6 @@ kubectl create -n kafka -f azure-kafka-kubernetes/kafka-operator-strimzi/kafkacl
 
 # Cleaning up local resources we downloaded
 rm -rf azure-kafka-kubernetes
+
+# finish all the steps
+echo 'Done creating kafka inside aks'
