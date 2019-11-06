99f26c4f7ee98fe7803acfe564ad6e62a04c346d
Vlad Kolesnikov <vladkol@microsoft.com>
2019-11-06 10:02:38 -0800

    Updated with Kafka trigger

---
+++ eventhubskafka-functions-cosmosdb/StreamingProcessor-CosmosDB-Test0/StreamingProcessor-CosmosDB/Test0.cs
@@ -0,0 +1,112 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.WebJobs.Extensions.Kafka;
+
+namespace StreamingProcessor
+{
+    public static class Test0
+    {
+        /*
+         * Cosmos DB output using Azure Function binding
+         */
+        [FunctionName("Test0")]
+        public static async Task RunAsync(
+            [KafkaTrigger("%EventHubName%", "default",
+            ConsumerGroup = "%ConsumerGroup%",
+            EventHubConnectionString = "EventHubsConnectionString")] KafkaEventData<string>[] kafkaEvents,
+            [CosmosDB(databaseName: "%CosmosDBDatabaseName%", collectionName: "%CosmosDBCollectionName%", ConnectionStringSetting = "CosmosDBConnectionString")] IAsyncCollector<JObject> cosmosMessage,
+            ILogger log)
+        {
+            var tasks = new List<Task>();
+            long len = 0;
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            foreach (var data in kafkaEvents)
+            {
+                try
+                {
+                    string message = data.Value;
+                    len += message.Length;
+
+                    var document = JObject.Parse(message);
+                    document["id"] = document["eventId"];
+                    document["enqueuedAt"] = data.Timestamp;
+                    document["processedAt"] = DateTime.UtcNow;
+
+                    tasks.Add(cosmosMessage.AddAsync(document));
+                }
+                catch (Exception ex)
+                {
+                    log.LogError($"{ex} - {ex.Message}");
+                }
+            }
+
+            await Task.WhenAll(tasks);
+
+            sw.Stop();
+
+            string logMessage = $"[Test0] T:{len} doc - E:{sw.ElapsedMilliseconds} msec";
+            if (len > 0)
+            {
+                logMessage += Environment.NewLine + $"AVG:{(sw.ElapsedMilliseconds / len):N3} msec";
+            }
+
+            log.LogInformation(logMessage);
+        }
+        /*
+         * Cosmos DB output using Azure Function binding
+         */
+        /* [FunctionName("Test0")]
+        public static async Task RunAsync(
+            [EventHubTrigger("%EventHubName%", Connection = "EventHubsConnectionString", ConsumerGroup = "%ConsumerGroup%")] EventData[] eventHubData,
+            [CosmosDB(databaseName: "%CosmosDBDatabaseName%", collectionName: "%CosmosDBCollectionName%", ConnectionStringSetting = "CosmosDBConnectionString")] IAsyncCollector<JObject> cosmosMessage,
+            ILogger log)
+        {
+            var tasks = new List<Task>();
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            foreach (var data in eventHubData)
+            {
+                try
+                {
+                    string message = Encoding.UTF8.GetString(data.Body.Array);
+
+                    var document = JObject.Parse(message);
+                    document["id"] = document["eventId"];
+                    document["enqueuedAt"] = data.SystemProperties.EnqueuedTimeUtc;
+                    document["processedAt"] = DateTime.UtcNow;
+
+                    tasks.Add(cosmosMessage.AddAsync(document));
+                }
+                catch (Exception ex)
+                {
+                    log.LogError($"{ex} - {ex.Message}");
+                }
+            }
+
+            await Task.WhenAll(tasks);
+
+            sw.Stop();
+
+            string logMessage = $"[Test0] T:{eventHubData.Length} doc - E:{sw.ElapsedMilliseconds} msec";
+            if (eventHubData.Length > 0)
+            {
+                logMessage += Environment.NewLine + $"AVG:{(sw.ElapsedMilliseconds / eventHubData.Length):N3} msec";
+            }
+
+            log.LogInformation(logMessage);
+        } */
+    }
+
+}
---
+++ eventhubskafka-functions-cosmosdb/StreamingProcessor-CosmosDB-Test1/StreamingProcessor-CosmosDB/CosmosDBClient.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using System.Threading;
+
+namespace StreamingProcessor
+{
+    public class CosmosDBClient
+    {
+        private static CosmosDBClient _instance = null;
+        static SemaphoreSlim semaphoreSlim = new SemaphoreSlim(1, 1);
+
+        private readonly DocumentClient _client = null;
+        private readonly Uri _collectionLink = null;
+
+        private CosmosDBClient(DocumentClient client, Uri collectionLink)
+        {
+            _client = client;
+            _collectionLink = collectionLink;
+        }
+
+        private static async Task<CosmosDBClient> CreateInstance()
+        {
+            string connectionString = Environment.GetEnvironmentVariable("CosmosDBConnectionString");
+            string[] parts = connectionString.Split(';');
+            string endpoint = parts[0].Replace("AccountEndpoint=", "");
+            string accountKey = parts[1].Replace("AccountKey=", "");
+
+            var serviceEndpoint = new Uri(endpoint);
+            var authKey = accountKey;
+
+            var client = new DocumentClient(serviceEndpoint, authKey,
+                new ConnectionPolicy
+                {
+                    ConnectionMode = ConnectionMode.Direct,
+                    ConnectionProtocol = Protocol.Tcp
+                });
+
+            var database = new Database { Id = Environment.GetEnvironmentVariable("CosmosDBDatabaseName") };
+            var databaseItem = await client.CreateDatabaseIfNotExistsAsync(database);
+            var databaseLink = UriFactory.CreateDatabaseUri(database.Id);
+
+            var collection = new DocumentCollection { Id = Environment.GetEnvironmentVariable("CosmosDBCollectionName") };
+            var collectionItem = await client.CreateDocumentCollectionIfNotExistsAsync(databaseLink, collection);
+            var collectionLink = UriFactory.CreateDocumentCollectionUri(database.Id, collection.Id);
+
+            return new CosmosDBClient(client, collectionLink);
+        }
+
+        public static async Task<CosmosDBClient> GetClient()
+        {
+            await semaphoreSlim.WaitAsync();
+            try
+            {
+                return _instance ?? (_instance = await CreateInstance());
+            } finally
+            {
+                semaphoreSlim.Release();
+            }
+        }
+
+        public async Task<ResourceResponse<Document>> CreateDocumentAsync(object payload)
+        {
+            return await _client.UpsertDocumentAsync(_collectionLink, payload);
+        }
+    }
+}
