cddb5d540b2095f5cebe54c051205d92ac7bc631
benjguin <benjamin@3-4.fr>
2019-11-07 18:21:03 +0000

    complex event processing running in docker-compose

--- eventhubskafka-flink-eventhubskafka/flink-kafka-consumer/src/main/java/com/microsoft/samples/flink/ComplexEventProcessingJob.java
+++ eventhubskafka-flink-eventhubskafka/flink-kafka-consumer/src/main/java/com/microsoft/samples/flink/ComplexEventProcessingJob.java
@@ -4,6 +4,7 @@ import com.microsoft.samples.flink.data.SampleRecord;
 import com.microsoft.samples.flink.data.SampleTag;
 import com.microsoft.samples.flink.utils.JsonMapperSchema;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.contrib.streaming.state.RocksDBStateBackend;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -11,8 +12,10 @@ import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrderness
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 
 public class ComplexEventProcessingJob {
     private static final int MAX_EVENT_DELAY = 60; // max delay for out of order events
@@ -22,15 +25,17 @@ public class ComplexEventProcessingJob {
         ParameterTool params = ParameterTool.fromArgs(args);
 
         StreamExecutionEnvironment env = StreamingJobCommon.createStreamExecutionEnvironment(params);
-        JsonMapperSchema schema = new JsonMapperSchema(SampleRecord.class);
-        FlinkKafkaConsumer011<SampleRecord> consumer = StreamingJobCommon.createKafkaConsumer(params, schema);
-        JsonMapperSchema schema2 = new JsonMapperSchema(SampleTag.class);
+        JsonMapperSchema<SampleRecord> schema = new JsonMapperSchema(SampleRecord.class);
+        FlinkKafkaConsumer011<ConsumerRecord<byte[], SampleRecord>> consumer = StreamingJobCommon.createKafkaConsumer(params, schema);
+        JsonMapperSchema<SampleTag> schema2 = new JsonMapperSchema(SampleTag.class);
         FlinkKafkaProducer011<SampleTag> producer = StreamingJobCommon.createKafkaProducer(params, schema2);
 
         // setup streaming execution environment
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
         // TODO env.enableCheckpointing
         env.getConfig().setAutoWatermarkInterval(1000);
+
+        //env.setStateBackend(new RocksDBStateBackend());
 
         // assign a timestamp extractor
         consumer.assignTimestampsAndWatermarks(new SampleRecordTSExtractor());
@@ -40,6 +45,7 @@ public class ComplexEventProcessingJob {
         // create a data stream
         DataStream<SampleTag> stream = env.addSource(consumer)
                 .rebalance()
+                .map(c->c.value())
                 .keyBy(r -> r.deviceId)
                 .process(new ComplexEventProcessingLogic());
 
@@ -49,14 +55,14 @@ public class ComplexEventProcessingJob {
 
     }
 
-    public static class SampleRecordTSExtractor extends BoundedOutOfOrdernessTimestampExtractor<SampleRecord> {
+    public static class SampleRecordTSExtractor extends BoundedOutOfOrdernessTimestampExtractor<ConsumerRecord<byte[], SampleRecord>> {
         public SampleRecordTSExtractor() {
             super(Time.seconds(MAX_EVENT_DELAY));
         }
 
         @Override
-        public long extractTimestamp(SampleRecord sampleRecord) {
-            return sampleRecord.createdAt.toEpochMilli();
+        public long extractTimestamp(ConsumerRecord<byte[], SampleRecord> sampleRecordConsumerRecord) {
+            return sampleRecordConsumerRecord.timestamp();
         }
     }
 }
--- eventhubskafka-flink-eventhubskafka/kubernetes/provision-aks-flink-cluster.sh
+++ eventhubskafka-flink-eventhubskafka/kubernetes/provision-aks-flink-cluster.sh
@@ -68,7 +68,7 @@ cp ../flink-kafka-consumer/target/assembly/flink-kafka-consumer-$1.jar $tmpdir/f
 az acr build --registry $ACR_NAME --resource-group $RESOURCE_GROUP \
   --image $ACR_NAME.azurecr.io/flink-job-$1:latest \
   --build-arg job_jar=flink-kafka-consumer-$1.jar \
-  --build-arg flink_version=$FLINK_VERSION \
+  --build-arg flink_version=$FLINK_VERSION-scala_$FLINK_SCALA_VERSION \
   $tmpdir/flink-job
 rm -r $tmpdir
 
