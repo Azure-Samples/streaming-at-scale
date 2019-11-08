2bdfe3d098dd86f9e419257687b04b50d54cfbaf
Alexandre Gattiker <algattik@users.noreply.github.com>
2019-11-08 13:25:22 -0800

    Speeded up VNET idempotency

--- components/azure-common/create-virtual-network.sh
+++ components/azure-common/create-virtual-network.sh
@@ -15,15 +15,21 @@ if ! az network vnet show -n $VNET_NAME -g $RESOURCE_GROUP -o none 2>/dev/null; 
     -o tsv >> log.txt
 fi
 
-az network vnet subnet create -g $RESOURCE_GROUP --vnet-name $VNET_NAME \
-  -n producers-subnet --address-prefixes 10.0.0.0/24 \
-  --delegations Microsoft.ContainerInstance/containerGroups \
-  -o tsv >> log.txt
+if ! az network vnet subnet show -g $RESOURCE_GROUP --vnet-name $VNET_NAME -n producers-subnet -o none 2>/dev/null; then
+  az network vnet subnet create -g $RESOURCE_GROUP --vnet-name $VNET_NAME \
+    -n producers-subnet --address-prefixes 10.0.0.0/24 \
+    --delegations Microsoft.ContainerInstance/containerGroups \
+    -o tsv >> log.txt
+fi
 
-az network vnet subnet create -g $RESOURCE_GROUP --vnet-name $VNET_NAME \
-  -n ingestion-subnet --address-prefixes 10.0.1.0/24 \
-  -o tsv >> log.txt
+if ! az network vnet subnet show -g $RESOURCE_GROUP --vnet-name $VNET_NAME -n ingestion-subnet -o none 2>/dev/null; then
+  az network vnet subnet create -g $RESOURCE_GROUP --vnet-name $VNET_NAME \
+    -n ingestion-subnet --address-prefixes 10.0.1.0/24 \
+    -o tsv >> log.txt
+fi
 
-az network vnet subnet create -g $RESOURCE_GROUP --vnet-name $VNET_NAME \
-  -n streaming-subnet --address-prefixes 10.0.2.0/24 \
-  -o tsv >> log.txt
+if ! az network vnet subnet show -g $RESOURCE_GROUP --vnet-name $VNET_NAME -n streaming-subnet -o none 2>/dev/null; then
+  az network vnet subnet create -g $RESOURCE_GROUP --vnet-name $VNET_NAME \
+    -n streaming-subnet --address-prefixes 10.0.2.0/24 \
+    -o tsv >> log.txt
+fi
