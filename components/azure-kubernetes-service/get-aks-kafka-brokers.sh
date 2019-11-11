c2f73b8dcf648548debc1435bb05802437bf6393
Alexandre Gattiker <algattik@users.noreply.github.com>
2019-11-11 09:38:59 +0100

    Wait until Kafka external LB IP is assigned

--- components/azure-kubernetes-service/get-aks-kafka-brokers.sh
+++ components/azure-kubernetes-service/get-aks-kafka-brokers.sh
@@ -4,10 +4,28 @@
 set -euo pipefail
 
 echo "reading Kafka Broker IP from AKS"
-kafka_ip=$(kubectl get services -n kafka my-cluster-kafka-external-bootstrap -o=jsonpath='{.status.loadBalancer.ingress[0].ip}{"\n"}')
+TIMEOUT=120
+for i in $(seq 1 $TIMEOUT); do
+  if kafka_ip=$(kubectl --context $AKS_CLUSTER get services -n kafka kcluster-kafka-external-bootstrap -o=jsonpath='{.status.loadBalancer.ingress[0].ip}{"\n"}'); then
+    if [ "$kafka_ip" != "" ]; then
+      break
+    fi
+    echo "Broker deployed but IP not yet available. Retrying..."
+  else
+    echo "Broker not yet deployed. Retrying..."
+  fi
+  sleep 10
+done
+if [ "$kafka_ip" == "" ]; then
+  echo "Could not find Kafka IP"
+  exit 1
+fi
+
 
 export KAFKA_BROKERS="$kafka_ip:9094"
 export KAFKA_SECURITY_PROTOCOL=PLAINTEXT
 export KAFKA_SASL_MECHANISM=
 export KAFKA_SASL_JAAS_CONFIG=
 export KAFKA_SASL_JAAS_CONFIG_DATABRICKS=
+
+echo ". Broker: $KAFKA_BROKERS"
