2edc10e6b307cc60bfac153708df2d1d99419f12
Davide Mauri <damauri@microsoft.com>
2019-12-16 12:32:01 +0100

    allow to test both BC or HS

--- components/azure-sql/provision/db/provision.sql
+++ components/azure-sql/provision/db/provision.sql
@@ -1,5 +1,7 @@
 DROP TABLE IF EXISTS [dbo].[rawdata];
+DROP TABLE IF EXISTS [dbo].[rawdata_cs];
 DROP PROCEDURE IF EXISTS [dbo].[stp_WriteData];
+DROP PROCEDURE IF EXISTS [dbo].[stp_WriteData_cs];
 DROP TYPE IF EXISTS [dbo].[payloadType];
 BEGIN TRY
 	DROP PARTITION SCHEME [ps_af];
@@ -15,7 +17,7 @@ GO
 
 ALTER DATABASE [streaming] SET AUTO_UPDATE_STATISTICS_ASYNC ON
 GO
- 
+
 CREATE PARTITION FUNCTION [pf_af](int) AS RANGE LEFT FOR VALUES (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
 GO
 
@@ -41,6 +43,9 @@ CREATE TYPE [dbo].[payloadType] AS TABLE
 )
 GO
 
+/*
+	ROWSTORE
+*/
 CREATE TABLE [dbo].[rawdata]
 (
 	[BatchId] [uniqueidentifier] NOT NULL,
@@ -85,6 +90,64 @@ BEGIN
 	declare @buid uniqueidentifier = newId() 
 
 	insert into dbo.rawdata 
+		([BatchId], [EventId], [Type], [DeviceId], [DeviceSequenceNumber], [CreatedAt], [Value], [ComplexData], [ProcessedAt], [EnqueuedAt], [PartitionId], [StoredAt])
+	select
+		@buid as BatchId, 	
+		[EventId], [Type], [DeviceId], [DeviceSequenceNumber], [CreatedAt], [Value], [ComplexData], [ProcessedAt], [EnqueuedAt], [PartitionId],
+		sysutcdatetime() as StoredAt
+	from
+		@payload
+	;
+END
+GO
+
+/*
+	COLUMNSTORE
+*/
+CREATE TABLE [dbo].[rawdata_cs]
+(
+	[BatchId] [uniqueidentifier] NOT NULL,
+	[EventId] [uniqueidentifier] NOT NULL DEFAULT NEWSEQUENTIALID(),
+	[Type] [varchar](10) NOT NULL,
+	[DeviceId] [varchar](100) NOT NULL,
+	[DeviceSequenceNumber] [bigint] NOT NULL,
+	[CreatedAt] [datetime2](7) NOT NULL,
+	[Value] [numeric](18, 0) NOT NULL,
+	[ComplexData] [nvarchar](max) NOT NULL,
+	[EnqueuedAt] [datetime2](7) NOT NULL,
+	[ProcessedAt] [datetime2](7) NOT NULL,
+	[StoredAt] [datetime2](7) NOT NULL,
+	[PartitionId] [int] NOT NULL
+) ON [ps_af]([PartitionId])
+GO
+
+ALTER TABLE [dbo].[rawdata_cs]  WITH CHECK ADD CHECK  ((isjson([ComplexData])=(1)))
+GO
+
+CREATE CLUSTERED COLUMNSTORE INDEX [ixccs] ON [dbo].[rawdata_cs] WITH (DROP_EXISTING = OFF, COMPRESSION_DELAY = 0) ON [ps_af]([PartitionId])
+GO
+
+ALTER TABLE dbo.[rawdata_cs] 
+ADD CONSTRAINT [pk__rawdata_cs] PRIMARY KEY NONCLUSTERED 
+	(
+		[EventId] ASC,
+		[PartitionId] ASC
+	)  WITH (DATA_COMPRESSION = PAGE) ON [ps_af]([PartitionId])
+GO
+
+CREATE NONCLUSTERED INDEX ix1 ON [dbo].[rawdata_cs] ([DeviceId], [DeviceSequenceNumber]) WITH (DATA_COMPRESSION = PAGE) ON [ps_af]([PartitionId])
+GO
+
+CREATE NONCLUSTERED INDEX ix2 ON [dbo].[rawdata_cs] ([BatchId]) WITH (DATA_COMPRESSION = PAGE) ON [ps_af]([PartitionId])
+GO
+
+CREATE OR ALTER PROCEDURE [dbo].[stp_WriteData_cs] 
+@payload AS dbo.payloadType READONLY
+AS
+BEGIN
+	declare @buid uniqueidentifier = NEWID() 
+
+	insert into dbo.rawdata_cs
 		([BatchId], [EventId], [Type], [DeviceId], [DeviceSequenceNumber], [CreatedAt], [Value], [ComplexData], [ProcessedAt], [EnqueuedAt], [PartitionId], [StoredAt])
 	select
 		@buid as BatchId, 	
