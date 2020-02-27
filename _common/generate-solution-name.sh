0ac581c2c50bf4d61a448146ee1425e4f292a942
Davide Mauri <damauri@microsoft.com>
2020-02-26 16:50:09 -0800

    fixed openssl usage

--- _common/generate-solution-name.sh
+++ _common/generate-solution-name.sh
@@ -3,4 +3,4 @@
 # Strict mode, fail on any error
 set -euo pipefail
 
-echo `openssl rand 5 -base64 | cut -c1-7 | tr '[:upper:]' '[:lower:]' | tr -cd '[[:alnum:]]._-'`
+echo `openssl rand -base64 5 | cut -c1-7 | tr '[:upper:]' '[:lower:]' | tr -cd '[[:alnum:]]._-'`
