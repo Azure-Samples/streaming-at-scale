dd8400270d3a8ff6bc383a50e65f6653f505c6d7
Davide Mauri <damauri@microsoft.com>
2020-09-25 15:11:14 -0700

    using desceding sort to facilitate time-series queries

--- components/azure-sql/provision/db/provision.sql
+++ components/azure-sql/provision/db/provision.sql
@@ -66,7 +66,7 @@ GO
 ALTER TABLE [dbo].[rawdata]  WITH NOCHECK ADD CHECK  ((isjson([ComplexData])=1))
 GO
 
-CREATE CLUSTERED INDEX [ixc] ON [dbo].[rawdata] ([StoredAt]) WITH (DATA_COMPRESSION = PAGE) ON [ps_af]([PartitionId])
+CREATE CLUSTERED INDEX [ixc] ON [dbo].[rawdata] ([StoredAt] DESC) WITH (DATA_COMPRESSION = PAGE) ON [ps_af]([PartitionId])
 GO
 
 ALTER TABLE dbo.[rawdata] 
