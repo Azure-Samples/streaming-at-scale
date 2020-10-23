59e7d9e7353e089dfceced503ee472fb7cda9190
Alexandre Gattiker <algattik@users.noreply.github.com>
2020-10-23 08:14:55 +0200

    .

--- components/azure-timeseriesinsights/create-timeseriesinsights.sh
+++ components/azure-timeseriesinsights/create-timeseriesinsights.sh
@@ -2,30 +2,36 @@
 
 set -euo pipefail
 
-accessPolicyContributorObjectIds=""
+echo 'creating TSI'
+
+az timeseriesinsights environment longterm create \
+  --resource-group $RESOURCE_GROUP \
+  --name $TSI_ENVIRONMENT \
+  --sku-name=L1 \
+  --storage-account-name=$AZURE_STORAGE_ACCOUNT \
+  --time-series-id-properties='[{"name":"deviceId", "type":"string"}]' \
+  -o tsv >>log.txt
+
+es_resource_id=$(az eventhubs eventhub get -g $RESOURCE_GROUP -n $EVENTHUB_NAME --namespace-name $EVENTHUB_NAMESPACE --query id --output tsv)
+
+az timeseriesinsights event-source eventhub create \
+  --resource-group $RESOURCE_GROUP \
+  --environment-name $TSI_ENVIRONMENT \
+  --name $EVENTHUB_NAME \
+  --event-source-resource-id $es_resource_id \
+  --consumer-group-name $EVENTHUB_CG \
+  --key-name Listen \
+  --timestamp-property-name createdAt \
+  -o tsv >>log.txt
 
 userType=$(az account show --query user.type -o tsv)
-
 if [ "$userType" == "user" ]; then
-
-  # Get ID of current user, to grant access to TSI environment
-  userId=$(az ad signed-in-user show --query objectId -o tsv)
-  accessPolicyContributorObjectIds='"'$userId'"'
-
+  echo 'Granting user access'
+  az timeseriesinsights access-policy create --environment-name
+    --resource-group $RESOURCE_GROUP \
+    --environment-name $TSI_ENVIRONMENT \
+    --name access \
+    --principal-object-id "$userId" \
+    --roles Contributor \
+    -o tsv >>log.txt
 fi
-
-echo 'creating TSI'
-az group deployment create \
-  --resource-group $RESOURCE_GROUP \
-  --template-file ../components/azure-timeseriesinsights/tsi-eventhubs-arm-template.json \
-  --parameters \
-  environmentName=$TSI_ENVIRONMENT \
-  eventHubNamespace=$EVENTHUB_NAMESPACE \
-  eventHubName=$EVENTHUB_NAME \
-  eventSourceKeyName=Listen \
-  consumerGroupName=$EVENTHUB_CG \
-  storageAccountName=$AZURE_STORAGE_ACCOUNT \
-  environmentTimeSeriesIdProperties='[{"name":"deviceId", "type":"string"}]' \
-  eventSourceTimestampPropertyName=createdAt \
-  accessPolicyContributorObjectIds='['$accessPolicyContributorObjectIds']' \
-  -o tsv >>log.txt
