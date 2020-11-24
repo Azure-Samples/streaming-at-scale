03ea4f66a0229352aadb3aae63f73abd8a97edda
Davide Mauri <damauri@microsoft.com>
2020-11-24 10:49:44 -0800

    added timeseries query sample

---
+++ eventhubs-streamanalytics-azuresql/time-series-query-sample.sql
@@ -0,0 +1,35 @@
+/*
+	database: streaming@dmiot002sql
+*/
+declare @p table (pid int);
+insert into @p values (1),(2),(3),(4),(5),(6),(7),(8),(9),(10),(11),(12),(13),(14),(15),(16);
+
+select top (100)
+	*
+from
+	@p p
+cross apply
+(
+	select top (100)
+		*
+	from
+		dbo.[rawdata2] r
+	where
+		r.[PartitionId] = p.pid
+	order by
+		[StoredAt] desc
+) t
+order by
+	[StoredAt] desc
+go
+
+select top (100)
+	*
+from
+	dbo.[rawdata2] r
+where
+	[r].[DeviceId] = 'contoso://device-id-154'
+and
+	r.[PartitionId] = 5
+order by
+	[StoredAt] desc
