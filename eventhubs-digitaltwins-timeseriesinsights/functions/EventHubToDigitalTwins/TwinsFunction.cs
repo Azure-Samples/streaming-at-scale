9e9f8618d327869a6b1d32cd3cd6c98f369abf15
Alexandre Gattiker <algattik@users.noreply.github.com>
2020-12-24 12:02:23 +0100

    Update TwinsFunction.cs

--- eventhubs-digitaltwins-timeseriesinsights/functions/EventHubToDigitalTwins/TwinsFunction.cs
+++ eventhubs-digitaltwins-timeseriesinsights/functions/EventHubToDigitalTwins/TwinsFunction.cs
@@ -2,7 +2,8 @@ namespace EventHubToDigitalTwins
 {
     using System;
     using System.Net.Http;
-    using System.Text;
+    using System.Text.Json;
+    using System.Threading.Tasks;
     using Azure;
     using Azure.Core.Pipeline;
     using Azure.DigitalTwins.Core;
@@ -10,20 +11,20 @@ namespace EventHubToDigitalTwins
     using Microsoft.Azure.EventHubs;
     using Microsoft.Azure.WebJobs;
     using Microsoft.Extensions.Logging;
-    using Newtonsoft.Json;
-    using Newtonsoft.Json.Linq;
 
     public class TwinsFunction
     {
         //Your Digital Twin URL is stored in an application setting in Azure Functions
-        private static readonly string AdtInstanceUrl = Environment.GetEnvironmentVariable("ADT_SERVICE_URL");
+        private static readonly string AdtInstanceUrl = Environment.GetEnvironmentVariable("ADT_SERVICE_URL") ??
+                                                        throw new InvalidOperationException(
+                                                            "Application setting \"ADT_SERVICE_URL\" not set");
+
         private static readonly HttpClient HttpClient = new HttpClient();
 
         [FunctionName("TwinsFunction")]
-        public async void Run([EventHubTrigger("", Connection = "EVENT_HUB")]
+        public async Task Run([EventHubTrigger("", Connection = "EVENT_HUB")]
             EventData eventData, ILogger log)
         {
-            if (AdtInstanceUrl == null) log.LogError("Application setting \"ADT_SERVICE_URL\" not set");
             try
             {
                 //Authenticate with Digital Twins
@@ -36,38 +37,21 @@ namespace EventHubToDigitalTwins
                     log.LogInformation(eventData.ToString());
 
                     // Reading deviceId and temperature for IoT Hub JSON
-                    JObject body =
-                        (JObject) JsonConvert.DeserializeObject(Encoding.UTF8.GetString(eventData.Body));
-                    var deviceId = (string) eventData.SystemProperties["iothub-connection-device-id"];
-                    var deviceType = (string) body["DeviceType"];
+                    var body = JsonDocument.Parse(eventData.Body).RootElement;
+                    var deviceId = body.GetProperty("deviceId").GetString();
+                    var deviceType = body.GetProperty("type").GetString();
                     log.LogInformation($"Device:{deviceId} DeviceType is:{deviceType}");
                     var updateTwinData = new JsonPatchDocument();
                     switch (deviceType)
                     {
-                        case "FanningSensor":
-                            updateTwinData.AppendAdd("/ChasisTemperature",
-                                body["ChasisTemperature"].Value<double>());
-                            updateTwinData.AppendAdd("/FanSpeed", body["Force"].Value<double>());
-                            updateTwinData.AppendAdd("/RoastingTime",
-                                body["RoastingTime"].Value<int>());
-                            updateTwinData.AppendAdd("/PowerUsage",
-                                body["PowerUsage"].Value<double>());
+                        case "TEMP":
+                            updateTwinData.AppendAdd("/Temperature", body.GetProperty("value").GetDouble());
+                            updateTwinData.AppendAdd("/TemperatureData", body.GetProperty("complexData"));
                             await client.UpdateDigitalTwinAsync(deviceId, updateTwinData);
                             break;
-                        case "GrindingSensor":
-                            updateTwinData.AppendAdd("/ChasisTemperature",
-                                body["ChasisTemperature"].Value<double>());
-                            updateTwinData.AppendAdd("/Force", body["Force"].Value<double>());
-                            updateTwinData.AppendAdd("/PowerUsage",
-                                body["PowerUsage"].Value<double>());
-                            updateTwinData.AppendAdd("/Vibration", body["Vibration"].Value<double>());
-                            await client.UpdateDigitalTwinAsync(deviceId, updateTwinData);
-                            break;
-                        case "MouldingSensor":
-                            updateTwinData.AppendAdd("/ChasisTemperature",
-                                body["ChasisTemperature"].Value<double>());
-                            updateTwinData.AppendAdd("/PowerUsage",
-                                body["PowerUsage"].Value<double>());
+                        case "CO2":
+                            updateTwinData.AppendAdd("/CO2", body.GetProperty("value").GetDouble());
+                            updateTwinData.AppendAdd("/CO2Data", body.GetProperty("complexData"));
                             await client.UpdateDigitalTwinAsync(deviceId, updateTwinData);
                             break;
                     }
