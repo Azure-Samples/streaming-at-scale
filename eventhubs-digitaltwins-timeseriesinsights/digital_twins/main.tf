87727468f0ac70a7fec3e8b65e170389786fb86e
Alexandre Gattiker <algattik@users.noreply.github.com>
2020-12-24 07:40:53 +0100

    Create main.tf

---
+++ eventhubs-digitaltwins-timeseriesinsights/digital_twins/main.tf
@@ -0,0 +1,20 @@
+resource "azurerm_storage_account" "time_series_insights" {
+  name                     = "st${var.basename}tsi"
+  location                 = var.location
+  resource_group_name      = var.resource_group
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_digital_twins_instance" "main" {
+  name                = "dt-${var.basename}"
+  location            = var.location
+  resource_group_name = var.resource_group
+}
+
+resource "azurerm_digital_twins_endpoint_eventhub" "main" {
+  name                                 = "EH"
+  digital_twins_id                     = azurerm_digital_twins_instance.main.id
+  eventhub_primary_connection_string   = var.eventhub_primary_connection_string
+  eventhub_secondary_connection_string = var.eventhub_secondary_connection_string
+}
