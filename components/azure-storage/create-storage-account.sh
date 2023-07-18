4396c3a0218f29b267774255e71887f986860f04
Alexandre Gattiker <algattik@microsoft.com>
2023-07-18 11:00:27 +0200

    Update create-storage-account.sh

--- components/azure-storage/create-storage-account.sh
+++ components/azure-storage/create-storage-account.sh
@@ -7,5 +7,5 @@ echo 'creating storage account'
 echo ". name: $AZURE_STORAGE_ACCOUNT"
 
 az storage account create -n $AZURE_STORAGE_ACCOUNT -g $RESOURCE_GROUP \
-    --kind StorageV2 --sku Standard_LRS \
+    --kind StorageV2 --sku Standard_LRS --allow-blob-public-access false \
     -o tsv >> log.txt
